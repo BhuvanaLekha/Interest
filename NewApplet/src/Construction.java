3f82284a5b143d8cf31979d7529f720f3f036762
BhuvanaLekha <60215882+BhuvanaLekha@users.noreply.github.com>
2020-02-19 17:12:14 +0530

    Update Construction.java

--- NewApplet/src/Construction.java
+++ NewApplet/src/Construction.java
@@ -12,25 +12,25 @@ public class Construction extends Applet implements ItemListener,ActionListener
   CheckboxGroup rb;
   Checkbox c,c1,c2,c3;
   Button b;
-  int ccost =1200;
-  int c1cost =1500;
-  int c2cost =1800;
-  int c3cost =2500;
+  int standardmaterials =1200;
+  int abovestandardmaterials =1500;
+  int highstandardmaterials =1800;
+  int highstandardmaterialsandfullyautomated =2500;
   int total_cost;
     @Override
     public void init()
     {
         l1=new Label("SELECT THE MATERIAL");
         l3=new Label("Enter the total Sq Feet");
-        t2= new TextField("200",50);
-        l2 = new Label("estimated cost is");
-        t1= new TextField("cost is",50);
+        t2= new TextField("",50);
+        l2 = new Label("Estimated cost is");
+        t1= new TextField("",50);
         rb= new CheckboxGroup();
          c=new Checkbox("Standard Materials", rb, true);
          c1=new Checkbox("above Standard Materials", rb, true);
          c2=new Checkbox("High Standard Materials",rb, true);
          c3=new Checkbox("Fully Automated",rb, true);
-           b=new Button("Subbmit"); 
+           b=new Button("Submit"); 
         add(l1);
         add(c);
         add(c1);
@@ -41,7 +41,6 @@ public class Construction extends Applet implements ItemListener,ActionListener
         add(b);
         add(l2);
         add(t1);
-        System.out.println("initialized");
 	c.addItemListener(this);
 	c1.addItemListener(this);
 	c2.addItemListener(this);
@@ -54,33 +53,34 @@ public class Construction extends Applet implements ItemListener,ActionListener
 {  
      if(c.getState()== true )
      {    
-        int s =Integer.parseInt(t2.getText());
-        total_cost = ccost*s;
+        String s1=t2.getText();
+        int s =Integer.parseInt(s1);
+        total_cost = standardmaterials*s;
         repaint();
      }
     else if(c1.getState()== true )
       {
-          System.out.println("initialized");
           String s1=t2.getText();
         int s =Integer.parseInt(s1);
-        System.out.println(s);
-        total_cost = c1cost*s; 
-        System.out.println(total_cost);
-         repaint(); 
+         total_cost =abovestandardmaterials*s; 
+          repaint(); 
       }
     else if(c2.getState()== true)
       {
-          int s =Integer.parseInt(t2.getText());
-        total_cost = c2cost*s;  
+          String s1=t2.getText();
+        int s =Integer.parseInt(s1);
+        total_cost = highstandardmaterials*s;  
          repaint(); 
       }
     else if(c3.getState()== true )
       {
-       int s =Integer.parseInt(t2.getText());
-        total_cost = c3cost*s;  
+      String s1=t2.getText();
+        int s =Integer.parseInt(s1);
+        total_cost =highstandardmaterialsandfullyautomated*s;  
         repaint();
       }	
-    else if (false){}
+    else if (false){
+    }
     
 }
 
@@ -92,30 +92,9 @@ public class Construction extends Applet implements ItemListener,ActionListener
             t1.setText(String.valueOf(total_cost));
         }  
     }  
-    @Override
-    public void start()
-    {
-        System.out.println("started");
-        
-    }
-    @Override
+     @Override
     public void paint(Graphics g)
     {
-         //System.out.println("paint called");
-         Checkbox chk = rb.getSelectedCheckbox(); 
-		
-	g.drawString(chk.getLabel()+ "is selected", 15 ,50);
-         
+                 Checkbox chk = rb.getSelectedCheckbox(); 
+	            }
     }
-    @Override
-    public void stop()
-    {
-       System.out.println("stopped");  
-    }
-    @Override
-    public void destroy()
-    {
-        System.out.println("destroyed"); 
-    }
-    
-}
